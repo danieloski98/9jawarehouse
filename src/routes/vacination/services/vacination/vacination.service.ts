ec19c05e87b45451f496e3187a160bcbe773fea0
Neon <danielemmanuel257@gmail.com>
2021-10-07 16:08:23 +0100

    uploading to cloudinary

--- src/routes/vacination/services/vacination/vacination.service.ts
+++ src/routes/vacination/services/vacination/vacination.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/Schema/User.schema';
 import { Vacination, VacinationDocument } from 'src/Schema/Vacination.Schema';
+import cloudinary from 'src/utils/cloudinary';
 import { Return } from 'src/utils/Returnfunctions';
 import { IReturnObject } from 'src/utils/ReturnObject';
 
@@ -32,7 +33,18 @@ export class VacinationService {
 
       if (vacineRecords.length < 1) {
         // create a new vacine record
-        const update = await this.vacineModel.create(record);
+        // upload the vacine image to cloudinary
+        const image = await cloudinary.uploader.upload(record.image_link, {
+          overwrite: true,
+          invalidate: true,
+          width: 810,
+          height: 456,
+          crop: 'fill',
+        });
+        const update = await this.vacineModel.create({
+          user_id: record.user_id,
+          image_link: image.secure_url,
+        });
         console.log(update);
         return Return({
           error: false,
