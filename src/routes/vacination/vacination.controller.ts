7100b7d53ae63cf43a7b3cd4eb01d324e095a641
Neon <danielemmanuel257@gmail.com>
2021-10-07 17:19:08 +0100

    console.logging the content type

--- src/routes/vacination/vacination.controller.ts
+++ src/routes/vacination/vacination.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Res, UseInterceptors, UploadedFile } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  UseInterceptors,
+  UploadedFile,
+  Req,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
@@ -14,12 +22,15 @@ export class VacinationController {
   @ApiTags('VACINATION')
   @ApiBody({ type: Vacination })
   async createRecord(
+    @Req() req: Request,
     @Res() res: Response,
     @Body() body: Vacination,
     @UploadedFile() file: any,
   ) {
     console.log(file);
+    console.log('content-type', req.headers);
     const result = await this.vacineService.createVacination(body);
+    console.log(result);
     res.status(result.statusCode).send(result);
   }
 }
