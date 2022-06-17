e1625e51cb6911850f9c99d2bd9fe85e1595cc53
= <=>
2022-06-17 04:47:53 +0100

    d

--- src/Schema/Notification.Schema.ts
+++ src/Schema/Notification.Schema.ts
@@ -33,7 +33,7 @@ export class Notification {
   @ApiProperty()
   @Prop({
     type: String,
-    default: new Date('2011-09-24T00:00:002011-09-24T00:00:00'),
+    default: new Date('2011-09-24T00:00:002011-09-24T00:00:00').toISOString(),
   })
   created_at: string;
 }
