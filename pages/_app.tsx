bff2fbf23ab50198440625446971f72ca587c5af
Neon <danielemmanuel257@gmail.com>
2022-06-28 18:07:43 +0100

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,6 +45,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           </Head>
 
             <FacebookPixel />
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+              <Script id="google" strategy="lazyOnload">
+                  {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-DB3RZYE00Y', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+              </Script>
 
           <Component {...pageProps} />
         </Provider>
