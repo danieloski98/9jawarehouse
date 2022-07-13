e23eb9757c5d7dd4101c86ace055fb22068b54d1
Neon <danielemmanuel257@gmail.com>
2022-07-13 13:37:56 +0100

    removed facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,16 +20,16 @@ function FacebookPixel() {
   const router = useRouter() 
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('748044199533092');
-        ReactPixel.pageView();
+    // import("react-facebook-pixel")
+    //   .then((x) => x.default)
+    //   .then((ReactPixel) => {
+    //     ReactPixel.init('748044199533092');
+    //     ReactPixel.pageView();
 
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
+    //     router.events.on("routeChangeComplete", () => {
+    //       ReactPixel.pageView();
+    //     });
+    //   });
   });
   return null;
 }
